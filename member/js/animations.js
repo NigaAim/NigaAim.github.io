18d57d0ebd944a1b4b8e1ac47a89f2e9e964bbbe
KiritoSword <zakkjpn@protonmail.com>
2021-11-17 09:30:33 +0900

    updated member pages

---
+++ member/js/animations.js
@@ -0,0 +1,43 @@
+window.sr = ScrollReveal()
+
+sr.reveal(".navbar", {
+    opacity: 0,
+    delay: 200,
+    duration: 1000
+});
+
+sr.reveal(".profile-section h1", {
+    opacity: 0,
+    delay: 200,
+    duration: 2000
+});
+
+sr.reveal(".profile-section h2", {
+    opacity: 0,
+    delay: 1000,
+    duration: 1500
+});
+
+sr.reveal(".profile-section img", {
+    opacity: 0,
+    delay: 1000,
+    duration: 1500
+});
+
+sr.reveal(".profile-section .context h2", {
+    opacity: 0,
+    delay: 200,
+    duration: 2000
+});
+
+sr.reveal(".profile-section p", {
+    opacity: 0,
+    delay: 1000,
+    duration: 1500
+});
+
+sr.reveal(".profile-section #twitter", {
+    opacity: 0,
+    delay: 1000,
+    duration: 1500
+});
